5a66c4e8ddfda78a37205c6e6b0e6788a3d11799
Theresa Stocking <nottodayjose@MacBook-Pro.attlocal.net>
2019-04-09 21:54:05 -0500

    Found student js error

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react';
+import {HashRouter as Router, Route, Switch, Link} from 'react-router-dom';
+import routes from "./routes";  
+// import student from "./components/Student/Student"
+// import Link from 'react-router-dom';
 
 export default class App extends Component {
   render() {
     return (
+    <Router> 
       <div>
         <nav className='nav'>
+        {/* <routes /> */}
           <div>WestSide University</div> 
           <div className='link-wrap'>
-              <div className='links'>Home</div>
-              <div className='links'>About</div> 
+              {/* <div className='links'>Home</div>
+              <div className='links'>About</div>  */}
+                <Link to="/" className="links">Home</Link>
+                <Link to="/about" className="links">About</Link>
           </div>
         </nav>
-      </div>
+    {routes}
+    
+    </div>
+    </Router>
+
     )
   }
 }
